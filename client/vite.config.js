710e4dd5973705129aa8095221874befebf6dfd0
Haimanot Gebre Shikur <haimanotg2@gmail.com>
2026-05-14 01:50:43 +0300

    Added PWA install support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 import {
   VitePWA,
 } from "vite-plugin-pwa";
@@ -14,6 +15,16 @@ export default defineConfig({
   
       registerType:
         "autoUpdate",
+  
+      includeAssets: [
+  
+        "favicon.png",
+  
+        "pwa-192.png",
+  
+        "pwa-512.png",
+  
+      ],
   
       manifest: {
   
@@ -41,18 +52,33 @@ export default defineConfig({
         start_url:
           "/",
   
+        scope:
+          "/",
+  
         icons: [
   
           {
+  
             src: "/pwa-192.png",
-            sizes: "192x192",
-            type: "image/png",
+  
+            sizes:
+              "192x192",
+  
+            type:
+              "image/png",
+  
           },
   
           {
+  
             src: "/pwa-512.png",
-            sizes: "512x512",
-            type: "image/png",
+  
+            sizes:
+              "512x512",
+  
+            type:
+              "image/png",
+  
           },
   
         ],
